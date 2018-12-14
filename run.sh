f3698d578e9c81210885f22ce4d4b2af43fbaace
Benedict Cairns <bc5g15@soton.ac.uk>
2018-12-14 10:22:00 +0000

    Added file checking in the bash script

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # Load credentials from local folder and start
-export GOOGLE_APPLICATION_CREDENTIALS=./firebase-test-222916-firebase-adminsdk-slzkf-826b7cb679.json
-dev_appserver.py .
+FILE=./firebase-test-222916-firebase-adminsdk-slzkf-826b7cb679.json
+if [ -f $FILE ]; then
+    # If file exists
+    export GOOGLE_APPLICATION_CREDENTIALS=$FILE
+    dev_appserver.py .
+else
+    echo "Keyfile not found! Download it from drive and place in this directory"
+fi
