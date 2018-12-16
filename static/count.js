6ddbff00816b90a32cdceda88d9c11d9e6d5514e
Benedict Cairns <bc5g15@soton.ac.uk>
2018-12-16 12:40:09 +0000

    Started adding count to use a custom data set

---
+++ static/count.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/**
+ * @fileoverview Multi-user counting
+ */
+
+/**
+ * @param gameKey - a unique key for this game
+ * @param me - my user id
+ * @param token - secure token passed from the server
+ * @param channelId - id of the 'channel' we'll be listening to
+ */
+
+function initGame(gameKey, me, token, channelId, initialMessage)
+{
+    var state = {
+        gameKey: gameKey,
+        me: me
+    };
+
+    // This is our Firebase realtime DB path that we'll listen to 
+    // for updates. Initialise later in openChannel()
+    var channel = null;
+
+    /**
+     * Updates the displayed number
+     */
+    function updateGame(newState)
+    {
+        // Nothing yet!
+    }
+
+
+    function openChannel() {
+        // [START auth_login]
+        // sign into Firebase with the token passed from the server
+        firebase.auth().signInWithCustomToken(token).catch(
+            function(error){
+                console.log('Login Failed: ', error.code);
+                console.log('Error message: ', error.message);
+            });
+        // [END auth_login]
+
+        // [START add_listener]
+        channel = firebase.database().ref('channels/' + channelId);
+        // add a listener to the path that fires any time the
+        // value of the data changes
+        channel.on('value', function(data) {
+            console.log("Change!");
+        });
+        // [END add_listener]
+    }
+
+    /**
+   * This function opens a communication channel with the server
+   * then it adds listeners to all the squares on the board
+   * next it pulls down the initial game state from template values
+   * finally it updates the game state with those values by calling onMessage()
+   */
+    function initialize() {
+        // Always include the gamekey in our requests
+        $.ajaxPrefilter(function(opts) {
+          if (opts.url.indexOf('?') > 0)
+            opts.url += '&g=' + state.gameKey;
+          else
+            opts.url += '?g=' + state.gameKey;
+        });
+    
+        $('#board').on('click', '.cell', moveInSquare);
+    
+        openChannel();
+    
+        onMessage(initialMessage);
+      }
+
+      setTimeout(initialize, 100);
+}
