4b1eb66ec754e482f7f1fb763528d1842286910b
MithihotepTheIndomitable <36106045+MithihotepTheIndomitable@users.noreply.github.com>
2018-12-20 15:15:41 +0000

    Fixed all known bugs so far

--- TypeTester/Main.py
+++ TypeTester/Main.py
@@ -9,15 +9,11 @@ longestwords = typetester.longestWords(text, longeststrings)
 lineratings = typetester.rateSpecialChars(text, linespecialchars)
 for line in text:
     line.rstrip("\n")
-    linedifficulty = typetester.calculateLineComplexity(text.index(line), longeststrings, linespecialchars)
+    linedifficulty = typetester.calculateLineComplexity(line, longeststrings, linespecialchars)
     lineswithpoints[line] = linedifficulty
 
-difficultycsv = open("Difficulties.csv", 'w')
-writer = csv.writer(difficultycsv)
-#print(lineswithpoints.items())
-#writer.writerows(lineswithpoints.items())
-for key, value in lineswithpoints.items():
-    print(key + "," + str(value) + "/n")
-    writer.writerow([key, value])
+with open("Difficulties.csv", 'w') as difficultycsv:
+    writer = csv.writer(difficultycsv)
+    writer.writerows(lineswithpoints.items())
 
     
--- TypeTester/Tester.py
+++ TypeTester/Tester.py
@@ -1,14 +1,11 @@
-
 class Tester:
 
-    specialchars = ["?", "!", ]
     text = []
 
     def retrieveText(self):
-        textfile = open("Short Text.txt", "r")
+        textfile = open("Text.txt", "r")
         if textfile.mode == 'r':
             text = textfile.readlines()  #Produces a list of lines in the text
-            print(len(text))
         return text;
 
     def longestWords(self, text, longeststrings):
@@ -25,16 +22,12 @@ class Tester:
                 if number > currentmax:
                     currentmax = number
                     currentmaxindex = wordlengths.values().index(number)
-
-            longeststring = wordlengths.keys()[currentmaxindex] #Finds the longest string in the line and stores it and its length in the longeststrings dictionary
-            longeststrings[longeststring] = currentmax
+            longeststrings[line] = currentmax #Finds the longest string in the line and stores its length alongside the line in the longeststrings dictionary
             counter += 1
             wordlengths = {} #Empties wordlengths ready to work with the strings of the next line
             currentmax = 0
             currentmaxindex = 0
 
-        #print(counter)
-        print(len(longeststrings.keys()))
         return longeststrings
 
     def rateSpecialChars(self, text, linespecialchars):
@@ -46,15 +39,11 @@ class Tester:
                         counter += 1 #Looks at each character in each string of the line and adds 1 to the counter if it is non-alphanumeric
             linespecialchars[line] = counter #Assigns the value of the counter as the number of special characters in the line before setting it to 0 again for the next line
             counter = 0
-        #print(len(linespecialchars.values()))
         return linespecialchars
 
-    def calculateLineComplexity(self, sentencenumber, longeststrings, linespecialchars): #Calculates a score for a particular line by adding the length of its longest string to the number of special characters in it
-        longeststringvalues = longeststrings.values()
-        longestwordlength = longeststringvalues[sentencenumber]
-        linespecialcharvalues = linespecialchars.values()
-        #print("(" + str(sentencenumber) + ", " + str(len(longeststringvalues)) + ")")
-        linerating = linespecialcharvalues[sentencenumber]
+    def calculateLineComplexity(self, line, longeststrings, linespecialchars): #Calculates a score for a particular line by adding the length of its longest string to the number of special characters in it
+        longestwordlength = longeststrings.get(line)
+        linerating = linespecialchars.get(line)
         return longestwordlength + linerating
 
 
