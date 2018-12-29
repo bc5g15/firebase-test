702439a3fd94098bf9bab9c167616be1175eade8
MithihotepTheIndomitable <36106045+MithihotepTheIndomitable@users.noreply.github.com>
2018-12-29 14:14:41 +0000

    Modified retrieve_task and cleaned file for PEP8

--- temp/typeloader.py
+++ temp/typeloader.py
@@ -2,11 +2,11 @@ from flask import Blueprint
 import json
 import random
 import logging
-#import Tester
 from temp.typemodel import TypeTask
 
 loader = Blueprint('loader', __name__, template_folder='templates')
 keylist = []
+
 
 @loader.route("/loadtemp")
 def loadtemp():
@@ -17,24 +17,6 @@ def loadtemp():
     So long as the data kind is in the database
     everything else should be fine
     """
-    #typetester = Tester.Tester()
-    #lineswithpoints = {}
-    #longeststrings = {}
-    #linespecialchars = {}
-    #text = typetester.retrieveText()
-    #text = typetester.addMarkers(text)
-    #text = text.replace("\n", " ")
-    #lines = text.split("<>")
-    #longestwords = typetester.longestWords(lines, longeststrings)
-    #lineratings = typetester.rateSpecialChars(lines, linespecialchars)
-    #for line in lines:
-        #line.rstrip("\n")
-        #linedifficulty = typetester.calculateLineComplexity(line, longeststrings, linespecialchars)
-        #lineswithpoints[line] = linedifficulty
-
-    #with open("Difficulties.json", 'w') as outfile:
-        #outfile.write(json.dumps(lineswithpoints))
-    #file text =
     text = open('temp/Difficulties.json', 'r').read()
     records = json.loads(text)
     index = 1
@@ -42,24 +24,30 @@ def loadtemp():
         logging.info(item)
         # mytext= item["text"]
         # mydifficulty = item["difficulty"]
-        tt = TypeTask(id=index, text=item["text"], difficulty=item["difficulty"]) #No headers "text" or "difficulty" at the moment
-        key = tt.put() #put() returns a key, so I have made sure to store this key in a list of keys that can be used to find and return TypeTasks later on
+        tt = TypeTask(id=index, text=item["text"], difficulty=item["difficulty"])  # No headers "text" or "difficulty"
+        # at the moment
+        key = tt.put()  # put() returns a key, so I have made sure to store this key in a list of keys that can be used
+        #  to find and return TypeTasks later on
         keylist.append(key)
         index += 1
 
     return "Loaded values " + str(records)
 
-def get_task(): #Non-route version to be imported by other functions
+
+def retrieve_task():  # Non-route version of get_task to be imported by other functions
     global keylist
-    typetaskindex = random.randint(0, len(keylist) - 1) #Randomly generates a key index from 0 to the maximum value (index is one greater than the length of the list so 2 has to be subtracted from it)
-    typetaskkey = keylist[typetaskindex] #Retrieves the key from the list
-    typetask = typetaskkey.get() #Uses the key to get the corresponding TypeTask from the database
-    return str(typetask)
+    typetaskindex = random.randint(0, len(keylist) - 1)  # Randomly generates a key index from 0 to the maximum value
+    # (index is one greater than the length of the list so 2 has to be subtracted from it)
+    typetaskkey = keylist[typetaskindex]  # Retrieves the key from the list
+    typetask = typetaskkey.get()  # Uses the key to get the corresponding TypeTask from the database
+    return typetask
+
 
 @loader.route("/gettask")
 def get_task():
     global keylist
-    typetaskindex = random.randint(0, len(keylist) - 1) #Randomly generates a key index from 0 to the maximum value (index is one greater than the length of the list so 2 has to be subtracted from it)
-    typetaskkey = keylist[typetaskindex] #Retrieves the key from the list
-    typetask = typetaskkey.get() #Uses the key to get the corresponding TypeTask from the database
+    typetaskindex = random.randint(0, len(keylist) - 1)  # Randomly generates a key index from 0 to the maximum value
+    # (index is one greater than the length of the list so 2 has to be subtracted from it)
+    typetaskkey = keylist[typetaskindex]  # Retrieves the key from the list
+    typetask = typetaskkey.get()  # Uses the key to get the corresponding TypeTask from the database
     return str(typetask)
