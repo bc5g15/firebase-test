0ad145420b1c59b0d8a93543eb6b96879fa1b940
MithihotepTheIndomitable <36106045+MithihotepTheIndomitable@users.noreply.github.com>
2018-12-29 12:32:16 +0000

    Added non-route version of get_task for importing

--- temp/typeloader.py
+++ temp/typeloader.py
@@ -49,6 +49,12 @@ def loadtemp():
 
     return "Loaded values " + str(records)
 
+def get_task(): #Non-route version to be imported by other functions
+    global keylist
+    typetaskindex = random.randint(0, len(keylist) - 1) #Randomly generates a key index from 0 to the maximum value (index is one greater than the length of the list so 2 has to be subtracted from it)
+    typetaskkey = keylist[typetaskindex] #Retrieves the key from the list
+    typetask = typetaskkey.get() #Uses the key to get the corresponding TypeTask from the database
+    return str(typetask)
 
 @loader.route("/gettask")
 def get_task():
