317ecdb9b32a6fc0d2412f8d10ec6e68fe462f15
Kirtan-Amin <ka5g16@soton.ac.uk>
2019-01-02 22:15:37 +0000

    typeChallengeInterface.js refactored

--- src/classes/typeChallengeInterface.js
+++ src/classes/typeChallengeInterface.js
@@ -1,87 +1,96 @@
 /* eslint-disable no-unused-vars */
 import * as PIXI from 'pixi.js';
 import injector from 'pixi-multistyle-text-esnext';
-injector(PIXI);
-const loader = PIXI.loader;
-const app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
 
-let finished = false;
-let input = '';
-let userInputTxt;
-let challenge;
-let correct = 0;
-let totalTyped = 0;
-let counter = 0;
+export default class typingChallenge {
+  constructor(app, c) {
+    injector(PIXI);
 
-const txtStyle = new PIXI.TextStyle({
-  fontFamily: 'Arial',
-  fontSize: 20,
-  fontWeight: 'bold'
-});
-
-window.onkeypress = e => {
-  processInput(e);
-};
-window.onkeydown = e => {
-  if (e.key == 'Backspace') {
-    processInput(e);
-  }
-};
-
-export default function typeChallengeInterface(c) {
-  document.body.appendChild(app.view);
-  challenge = c;
-  showChallenge(challenge);
-}
-
-function processInput(event) {
-  if (event.key == 'Backspace') {
-    removeLastChar();
-    counter--;
-  } else if (event.key) {
-    validateInput(event.key);
-    counter++;
-    totalTyped++;
-  }
-
-  if (counter == challenge.length) {
-    let msg = new PIXI.Text(
-      'Challenge complete your accuracy was ' + correct / totalTyped * 100 + '%'
-    );
-    msg.y = 100;
-    app.stage.addChild(msg);
-  }
-}
-
-function showChallenge(challenge) {
-  userInputTxt = new PIXI.MultiStyleText(input, {
-    default: {
+    const txtStyle = new PIXI.TextStyle({
       fontFamily: 'Arial',
-      fontSize: '20px',
-      fill: '#cccccc',
-      align: 'center'
-    },
-    incorrect: {
-      fill: '#ff8888'
-    }
-  });
-  userInputTxt.y = 50;
+      fontSize: 20,
+      fontWeight: 'bold'
+    });
 
-  app.stage.addChild(new PIXI.Text(challenge, txtStyle), userInputTxt);
-}
+    this.app = app;
+    this.chalContainer = new PIXI.Container();
+    this.challenge = c;
+    this.finished = false;
+    this.input = '';
+    this.userInput;
+    this.correct = 0;
+    this.totalTyped = 0;
+    this.counter = 0;
 
-function validateInput(char) {
-  if (char != challenge.charAt(counter) && !finished) {
-    input += '<incorrect>' + char + '</incorrect>';
-    userInputTxt.text = input;
-  } else {
-    input += char;
-    userInputTxt.text = input;
-    correct++;
+    this.app.stage.addChild(this.chalContainer);
+
+    window.onkeypress = e => {
+      this.processInput(e);
+    };
+    window.onkeydown = e => {
+      if (e.key == 'Backspace') {
+        this.processInput(e);
+      }
+    };
+    document.body.appendChild(app.view);
   }
-}
 
-function removeLastChar() {
-  input = input.substr(0, input.length - 1);
-  userInputTxt.text = input;
+  processInput(event) {
+    if (event.key == 'Backspace') {
+      this.removeLastChar();
+      this.counter--;
+    } else if (event.key) {
+      this.validateInput(event.key);
+      this.counter++;
+      this.totalTyped++;
+    }
+
+    if (this.counter == this.challenge.length) {
+      let msg = new PIXI.Text(
+        'Challenge complete your accuracy was ' + this.calculateAccuracy() + '%'
+      );
+      msg.y = 100;
+      app.stage.addChild(msg);
+    }
+  }
+
+  showChallenge(challenge) {
+    this.userInputTxt = new PIXI.MultiStyleText(this.input, {
+      default: {
+        fontFamily: 'Arial',
+        fontSize: '20px',
+        fill: '#cccccc',
+        align: 'center'
+      },
+      incorrect: {
+        fill: '#ff8888'
+      }
+    });
+    this.userInputTxt.y = 50;
+
+    this.chalContainer.addChild(
+      new PIXI.Text(challenge, this.txtStyle),
+      this.userInputTxt
+    );
+  }
+
+  validateInput(char) {
+    if (char != this.challenge.charAt(this.counter) && !this.finished) {
+      this.input += '<incorrect>' + char + '</incorrect>';
+      this.userInputTxt.text = this.input;
+    } else {
+      this.input += char;
+      this.userInputTxt.text = this.input;
+      this.correct++;
+    }
+  }
+
+  calculateAccuracy() {
+    return this.correct / this.totalTyped * 100;
+  }
+
+  removeLastChar() {
+    this.input = this.input.substr(0, this.input.length - 1);
+    this.userInputTxt.text = this.input;
+  }
 }
