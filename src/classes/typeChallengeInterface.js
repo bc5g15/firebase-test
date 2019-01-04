cb0fdeb45d69946f80c98822bb8e61ac023b3893
Kirtan-Amin <ka5g16@soton.ac.uk>
2019-01-04 11:26:01 +0000

    Bug in removing last character fixed

--- src/classes/typeChallengeInterface.js
+++ src/classes/typeChallengeInterface.js
@@ -2,8 +2,8 @@
 import * as PIXI from 'pixi.js';
 import injector from 'pixi-multistyle-text-esnext';
 
-export default class typingChallenge {
-  constructor(app, c) {
+export default class TypingChallenge {
+  constructor(app, callback, chal) {
     injector(PIXI);
 
     const txtStyle = new PIXI.TextStyle({
@@ -11,10 +11,21 @@ export default class typingChallenge {
       fontSize: 20,
       fontWeight: 'bold'
     });
+    let bg = new PIXI.Graphics();
+    bg.beginFill(0x00000);
+    bg.lineStyle(2, 0xffffff);
+    bg.drawRect(0, 0, 800, 770);
+    let bg2 = new PIXI.Graphics();
+    bg2.beginFill(0x000000);
+    bg2.lineStyle(2, 0xffffff);
+    bg2.drawRect(0, 400, 800, 770 / 2);
 
     this.app = app;
     this.chalContainer = new PIXI.Container();
-    this.challenge = c;
+    this.inputContainer = new PIXI.Container();
+    this.callBack = callback;
+    this.challenge = chal;
+    console.log('challenge in constructor: ', this.challenge);
     this.finished = false;
     this.input = '';
     this.userInput;
@@ -22,7 +33,10 @@ export default class typingChallenge {
     this.totalTyped = 0;
     this.counter = 0;
 
-    this.app.stage.addChild(this.chalContainer);
+    this.app.stage.addChild(this.chalContainer, this.inputContainer);
+    this.chalContainer.addChild(bg);
+    this.inputContainer.addChild(bg2);
+    this.inputContainer.position.y = this.chalContainer.position.y / 2;
 
     window.onkeypress = e => {
       this.processInput(e);
@@ -49,12 +63,25 @@ export default class typingChallenge {
       let msg = new PIXI.Text(
         'Challenge complete your accuracy was ' + this.calculateAccuracy() + '%'
       );
+
       msg.y = 100;
-      app.stage.addChild(msg);
+      this.chalContainer.addChild(msg);
+      this.finished = true;
+      if (this.calculateAccuracy() < 0.5) {
+        alert(
+          'cannot fire, accuracy was below 50%, you had ' +
+            this.calculateAccuracy() +
+            '% accuracy'
+        );
+      } else {
+        this.callBack();
+      }
+      this.app.stage.removeChild(this.chalContainer);
+      this.app.stage.removeChild(this.inputContainer);
     }
   }
 
-  showChallenge(challenge) {
+  showChallenge() {
     this.userInputTxt = new PIXI.MultiStyleText(this.input, {
       default: {
         fontFamily: 'Arial',
@@ -62,21 +89,27 @@ export default class typingChallenge {
         fill: '#cccccc',
         align: 'center'
       },
-      incorrect: {
+      w: {
         fill: '#ff8888'
       }
     });
-    this.userInputTxt.y = 50;
+    this.userInputTxt.y = 400;
+    let tmp = new PIXI.MultiStyleText(this.challenge, {
+      default: {
+        fontFamily: 'Arial',
+        fontSize: '20px',
+        fill: '#cccccc',
+        align: 'center'
+      }
+    });
 
-    this.chalContainer.addChild(
-      new PIXI.Text(challenge, this.txtStyle),
-      this.userInputTxt
-    );
+    this.chalContainer.addChild(tmp);
+    this.inputContainer.addChild(this.userInputTxt);
   }
 
   validateInput(char) {
     if (char != this.challenge.charAt(this.counter) && !this.finished) {
-      this.input += '<incorrect>' + char + '</incorrect>';
+      this.input += '<w>' + char + '</w>';
       this.userInputTxt.text = this.input;
     } else {
       this.input += char;
@@ -90,15 +123,25 @@ export default class typingChallenge {
   }
 
   isComplete() {
+    let isFinished = this.finished;
     return new Promise(function(resolve, reject) {
-      if (this.finished) {
+      console.log('inside promise isFinished: ', isFinished);
+      if (isFinished) {
         resolve();
       }
     });
   }
 
   removeLastChar() {
-    this.input = this.input.substr(0, this.input.length - 1);
+    if (this.input.substr(this.input.length - 4, this.input.length) == '</w>') {
+      this.input = this.input.substr(0, this.input.length - 8);
+    } else {
+      console.log(
+        'removing last letter: ',
+        this.input.substr(0, this.input.length - 1)
+      );
+      this.input = this.input.substr(0, this.input.length - 1);
+    }
     this.userInputTxt.text = this.input;
   }
 }
