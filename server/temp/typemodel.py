8873f2adc896555b8b48f4b08a72e751a399bda0
Benedict Cairns <bc5g15@soton.ac.uk>
2019-01-05 20:42:31 +0000

    Added comment and line for PEP8

--- server/temp/typemodel.py
+++ server/temp/typemodel.py
@@ -1,5 +1,10 @@
 from google.appengine.ext import ndb
 
+
 class TypeTask(ndb.Model):
+    """
+    Represents the database state of a
+    typing task
+    """
     text = ndb.StringProperty()
     difficulty = ndb.IntegerProperty()
