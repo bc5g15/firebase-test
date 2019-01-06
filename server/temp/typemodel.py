1a0de98419958d7ad93efbf322f6d8540e8bf27f
Benedict Cairns <bc5g15@soton.ac.uk>
2019-01-06 23:37:16 +0000

    Code cleaning and PEP8 matching

--- server/temp/typemodel.py
+++ server/temp/typemodel.py
@@ -1,5 +1,9 @@
 from google.appengine.ext import ndb
 
+
 class TypeTask(ndb.Model):
+    """
+    Stores information relevant to typing challenges
+    """
     text = ndb.StringProperty()
     difficulty = ndb.IntegerProperty()
